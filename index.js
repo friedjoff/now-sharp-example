e9c7950f1ac6666eddd359c354ec12b549b3e9a2
Friedjoff Trautwein <mail@friedjoff.de>
2019-04-04 09:49:13 +0200

    Download image first.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const sharp = require('sharp')
+const download = require("download");
+const { tmpdir } = require("os");
+const sharp = require("sharp");
+
+const url =
+  "https://raw.githubusercontent.com/technopagan/sqip/master/demo/monkey-selfie.jpg";
 
 module.exports = async (req, res) => {
-  const image = sharp('input.jpg').resize({
+  await download(url, tmpdir());
+  const image = sharp(tmpdir() + "/" + "monkey-selfie.jpg").resize({
     height: 100,
     width: 100
-  })
-  res.writeHead(200, { 'Content-Type': 'image/jpeg' })
-  res.end(await image.jpeg().toBuffer())
+  });
+  res.writeHead(200, { "Content-Type": "image/jpeg" });
+  res.end(await image.jpeg().toBuffer());
 };
